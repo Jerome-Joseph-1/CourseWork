d91e4654f2a12eacdd968067a77404a87b3e04bf
Jerome Joseph <jeromejoseph5813@gmail.com>
2024-07-29 03:13:55 +0530

    Updated template

--- Algorithms/PrepTerm/.template/template.cpp
+++ Algorithms/PrepTerm/.template/template.cpp
@@ -2,11 +2,34 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <set>
+#include <string>
 
 using namespace std;
 
+string GREEN = "\033[32m";
+string RED = "\033[31m";
+string WHITE = "\033[37m";
+
+void judge() {
+    bool is_correct = false;
+    // logic to check if answer is correct 
+    if (is_correct) {
+        cout << "Test Case: " << GREEN << "Passed" << "\n" << WHITE;
+    } else {
+        cout << "Test Case: " << RED << "Failed" << "\n" << WHITE;
+    }
+}
+
 void solve() {
-    
+    bool verify_answers = false;
+
+    if (verify_answers) {
+        judge();
+    } else {
+        
+    }
 }
 
 int main() {
